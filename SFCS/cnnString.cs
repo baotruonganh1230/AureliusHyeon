8939ee5d7723beb2ebaa02f7e6476b34acc5a290
baotruonganh1230 <46068133+baotruonganh1230@users.noreply.github.com>
2021-03-23 09:42:04 +0700

    Update cnnString.cs

--- SFCS/cnnString.cs
+++ SFCS/cnnString.cs
@@ -1,14 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace SFCS
 {
     class cnnString
 
-    { public SqlConnection cnn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Admin\Documents\SFCSDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+    { public SqlConnection cnn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Directory.GetCurrentDirectory() + "\\SFCSDatabase.mdf;Integrated Security=True;Connect Timeout=30");
     }
 }
